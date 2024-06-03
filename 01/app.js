8c453a1f104d13d2f85e24f87088a402f7377917
szkoti <120187555+szkoti@users.noreply.github.com>
2024-06-03 21:33:48 +0200

    create object

---
+++ 01/app.js
@@ -0,0 +1,20 @@
+
+// Utwórz obiekt, który będzie przypisany do zmiennej `user`. 
+
+// Obiekt ten ma posiadać 4 właściwości:
+// * firstName,
+// * lastName,
+// * sex,
+// * age.
+
+// Następnie wartość każdej właściwości obiektu wyświetl w konsoli.
+
+
+const user = {
+    firstName: "Pupek",
+    lastName: "Kochany",
+    sex: "male",
+    age: 2
+};
+
+console.log(user)
