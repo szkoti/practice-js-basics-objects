65c505fe4624b889a09ab393eaf97c48b559bb09
szkoti <120187555+szkoti@users.noreply.github.com>
2024-06-04 13:14:43 +0200

    for ... in Object

--- 02/app.js
+++ 02/app.js
@@ -1,3 +1,14 @@
+// W pliku `app.js` znajdziesz zmienną `calendarJS`, która zawiera informacje o wydaniach kolejnych 
+// wersji standardu języka JavaScript.
+// Twoim zadaniem jest wyświetlić te dane w konsoli przy użyciu pętli `for...in`.
+// Przykładowo dla pierwszego wydania tekst będzie wyglądał następująco: `ES1 wydano w terminie 1997-06`.
+// Jeśli wartość dla danej właściwości będzie równa `null` (tak jak w przypadku ES4), 
+// to tekst w konsoli ma się prezentować w ten sposób: `ES4 nie zostało wydane`.
+// Pamiętaj, że do wartości właściwości możesz się odwołać przy pomocy konstrukcji 
+// `const valueFromObjectProp = object[variableWithPropName]`.
+
+
+
 const calendarJS = {
     'ES1': '1997-06',
     'ES2': '1998-06',
@@ -9,3 +20,11 @@ const calendarJS = {
     'ES8': '2017-06',
     'ES9': '2018-06',
 }
+
+for (let key in calendarJS) {
+    if (calendarJS[key] === null) {
+        console.log( `${key} nie zostało wydane` );
+    } else {
+        console.log( `${key} wydano w terminie ${calendarJS[key]}` );
+    };
+};
