a6ead7d79ba46c04eeb392e4764f6608b0767a28
szkoti <120187555+szkoti@users.noreply.github.com>
2024-06-04 14:38:40 +0200

    birthday msg based on current date & object

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,22 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '04',
+        month: '06',
         year: '1985'
     }
 }
+
+console.log(parseInt(user.born.day), Number(user.born.month));
+
+const currentDate = new Date();
+const currDay = currentDate.getDate();
+const currMth = currentDate.getMonth() + 1;
+
+console.log( currDay, currMth);
+
+
+if (parseInt(user.born.day) === currDay && Number(user.born.month) === currMth) {
+    console.log(`Happy birthday ${user.firstName}!`)
+    };
+
